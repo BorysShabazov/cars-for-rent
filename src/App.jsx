c95be5e3d9b1034149fc548376a3001606e2f041
BorysShabazov <122737738+BorysShabazov@users.noreply.github.com>
2023-12-07 21:40:49 +0100

    add-routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { NavLink, Outlet, Route, Routes } from "react-router-dom";
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <NavLink to="/">home</NavLink>
+              <NavLink to="catalog">catalog</NavLink>
+              <NavLink to="favorites">favorites</NavLink>
+              {<Outlet />}
+            </div>
+          }
+        >
+          <Route index element={<div>main 1243</div>} />
+          <Route path="catalog" element={<div>catalog page</div>} />
+          <Route path="favorites" element={<div>favorites page</div>} />
+          <Route path="*" element={<div>not found</div>} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
